6294583d9b31c5b2cb8730c7656ce3cf0e0eac6d
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-23 10:26:22 +0530

    Show the Demo from App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { BarGraph } from "./Demo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">        
+      <header className="App-header">  
+        <BarGraph/>      
       </header>
     </div>
   );
---
+++ src/Demo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function BarGraph() {
+    return (
+        <div>
+            This is Demo
+        </div>
+    )
+}
