f0e4c251c1e46ff1f6cf2b8b32e7731770cad647
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-24 10:33:40 +0530

    Add read user input field.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import React from 'react';
 import './App.css';
-// import { BarGraph } from "./Demo";
 import Network from "./Network";
 
+function parseInputValue(text) {
+  // Update the nodes
+}
+
 function App() {
+  const [inputValue, setInputValue] = React.useState("placeholder text");
+
+  React.useEffect(() => {
+    console.log("Input value change");
+  }, [inputValue]);
+
   return (
     <div className="App">
-      <header className="App-header">  
-        {/* <BarGraph/> */}
+      <form action="/action_page.php">
+        <label for="fname">First name: </label>
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          value={inputValue}
+          onChange={event => {
+            setInputValue(event.target.value);
+          }}
+        />
+      </form>
         <Network width={500} height={500}/>
-      </header>
     </div>
   );
 }
