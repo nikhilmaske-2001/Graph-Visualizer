65b9c79072a7dbe7359a4ff38d382d3351f75e25
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-25 14:15:32 +0530

    Improve error handling and corner cases.

--- src/parser/parseUtils.ts
+++ src/parser/parseUtils.ts
@@ -4,6 +4,7 @@ export function parsePairs(input: string, directed = true): any {
     // trim whitespace
     // TODO: Error handling
     input = input.trim();
+    if(input.length < 2) throw new Error("Input too short");
     input = input.slice(1, input.length - 1);
 
     const links= [];
@@ -13,23 +14,39 @@ export function parsePairs(input: string, directed = true): any {
     let nextOpenBracket = input.indexOf("[", startInd);
     while(nextOpenBracket !== -1) {
       const nextCloseBracket = input.indexOf("]", nextOpenBracket);
-      const pair = getDirectedPair(
-        input.slice(nextOpenBracket+1, nextCloseBracket),
-        nodeSet
-      );
-      links.push(pair);
+      if(nextCloseBracket === -1) throw new Error("No matching closing bracket");
+
+      try {
+        const pair = getDirectedPair(
+          input.slice(nextOpenBracket+1, nextCloseBracket),
+          nodeSet
+        );
+        links.push(pair);
+      } catch (error) {
+        throw error;
+      }
 
       startInd = nextCloseBracket;
       nextOpenBracket = input.indexOf("[", startInd);
     }
+
+    if(links.length === 0) throw new Error("No pairs found");
+
     return {nodeSet: nodeSet, links: links};
   }
 
 function getDirectedPair(s: string, nodeSet: Set<string>) {
   s = s.trim();
+
+  if(s.length === 0 || s.indexOf(",") === -1) 
+    throw new Error("Pair needs two arguments");
+
   const sp = s.split(",");
   const src = sp[0].trim();
   const trg = sp[1].trim();
+
+  if(src.length === 0 || trg.length === 0) 
+    throw new Error("src and trg needs to be non-empty");
 
   nodeSet.add(src);
   nodeSet.add(trg);
